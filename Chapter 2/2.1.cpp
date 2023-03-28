3e4a8959d6eb5327f8efb219ba2dc9174193fe6f
pparfirev <99344548+pparfirev@users.noreply.github.com>
2023-03-28 17:56:02 +0300

    Add files via upload

---
+++ Chapter 2/2.1.cpp
@@ -0,0 +1,11 @@
+/* Ќапишите программу на C++, котора€ отобразит ваше им€ и адрес (можете
+указать фиктивные данные). */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Sherlock Holmes. Baker st. 221b" << endl;
+    return 0;
+}
